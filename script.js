72c592671fa2ae5132033d5b48a1bb806c6beabd
segawa <>
2022-01-28 19:25:40 +0900

    The previously selected object angle will be reflected.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 window.onload = function () {
 
   var canvas = new fabric.Canvas('canvas');
+
   canvas.setWidth(1000);
   canvas.setHeight(1000);
+
+  const hideHandle = (oImg) => {
+    oImg.setControlVisible('mb', false)
+    oImg.setControlVisible('mt', false)
+    oImg.setControlVisible('ml', false)
+    oImg.setControlVisible('mr', false)
+  }
+
   fabric.Image.fromURL('./sample.png', function (oImg) {
     oImg.set({
       originX: 'center',
       originY: 'center',
     })
     canvas.add(oImg)
+    hideHandle(oImg)
   })
+
   fabric.Image.fromURL('./sample.png', function (oImg) {
     oImg.set({
       top: 200,
@@ -18,57 +29,58 @@ window.onload = function () {
       originY: 'center',
     })
     canvas.add(oImg)
+    hideHandle(oImg)
   })
+
   canvas.renderAll();
 
-    document.body.addEventListener('touchmove', function(event) {
-      event.preventDefault();
-    }, false);
-    let stage = canvas.upperCanvasEl;
-    
-    let mc = new Hammer.Manager(stage);
-    let pan = new Hammer.Pan();
-    let rotate = new Hammer.Rotate();
-    let pinch = new Hammer.Pinch();
-    
-    mc.add([pan, pinch, rotate]);
-    mc.get('pinch').set({ enable: true });
-    mc.get('rotate').set({ enable: true });
-    
-    let adjustDeltaX = 0;
-    let adjustDeltaY = 0;
-    let adjustScale = 1;
-    let adjustRotation = 0;
-    
-    let currentDeltaX = null;
-    let currentDeltaY = null;
-    let currentScale = null;
-    let currentRotation = null;
-    
-    mc.on("panstart pinchstart rotatestart", function(e) {
-      const activeObject = canvas.getActiveObject()
-      adjustRotation -= e.rotation // TODO:前選択した角度が反映されてしまう。
-      adjustScale = activeObject.scaleX
-    });
-    
-    mc.on("panmove pinchmove rotatemove", function(e) {
-      currentRotation = adjustRotation + e.rotation;
-      currentScale = adjustScale * e.scale;
-      currentDeltaX = adjustDeltaX + (e.deltaX / currentScale);
-      currentDeltaY = adjustDeltaY + (e.deltaY / currentScale);
+  document.body.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+  }, false);
+  let stage = canvas.upperCanvasEl;
+  
+  let mc = new Hammer.Manager(stage);
+  let pan = new Hammer.Pan();
+  let rotate = new Hammer.Rotate();
+  let pinch = new Hammer.Pinch();
+  
+  mc.add([pan, pinch, rotate]);
+  mc.get('pinch').set({ enable: true });
+  mc.get('rotate').set({ enable: true });
+  
+  let adjustDeltaX = 0;
+  let adjustDeltaY = 0;
+  let adjustScale = 1;
+  let adjustRotation = 0;
+  
+  let currentDeltaX = null;
+  let currentDeltaY = null;
+  let currentScale = null;
+  let currentRotation = null;
+  
+  mc.on("panstart pinchstart rotatestart", function(e) {
+    const activeObject = canvas.getActiveObject()
+    adjustRotation -= e.rotation // The previously selected object angle will be reflected.
+    adjustScale = activeObject.scaleX
+  });
+  
+  mc.on("panmove pinchmove rotatemove", function(e) {
+    currentRotation = adjustRotation + e.rotation;
+    currentScale = adjustScale * e.scale;
+    currentDeltaX = adjustDeltaX + (e.deltaX / currentScale);
+    currentDeltaY = adjustDeltaY + (e.deltaY / currentScale);
 
-      const activeObject = canvas.getActiveObject()
-      activeObject.angle = Math.round(currentRotation) 
-      activeObject.scaleX = currentScale
-      activeObject.scaleY = currentScale
-    });
-    
-    mc.on("panend pinchend rotateend", function(e) {
-      adjustScale = currentScale;
-      adjustRotation = currentRotation;
-      adjustDeltaX = currentDeltaX;
-      adjustDeltaY = currentDeltaY;
-    });
+    const activeObject = canvas.getActiveObject()
+    activeObject.angle = Math.round(currentRotation) 
+    activeObject.scaleX = currentScale
+    activeObject.scaleY = currentScale
+  });
+  
+  mc.on("panend pinchend rotateend", function(e) {
+    adjustScale = currentScale;
+    adjustRotation = currentRotation;
+    adjustDeltaX = currentDeltaX;
+    adjustDeltaY = currentDeltaY;
+  });
 
-
-  }
+}
